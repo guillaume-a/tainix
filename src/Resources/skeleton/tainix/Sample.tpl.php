9c95cf2a92ecf8cac4ee234b4511651fdf5936c4
Guillaume Aveline <917449+guillaume-a@users.noreply.github.com>
2024-12-15 02:56:31 +0100

    Fix Sample and Game generation with arrays (remove quotes)

--- src/Resources/skeleton/tainix/Sample.tpl.php
+++ src/Resources/skeleton/tainix/Sample.tpl.php
@@ -9,13 +9,13 @@ class <?php echo $class_name; ?> implements SampleInterface
 {
 public function inputs(): InputInterface {
 return new <?php echo $input_class; ?>(
-<?php foreach ($inputs as $key => $value): ?>
-    <?php if(\is_array($value)): ?>
-        <?php echo \sprintf('%s: \'%s\',', $key, var_export($value, true)); ?>
-    <?php else: ?>
+<?php foreach ($inputs as $key => $value) { ?>
+    <?php if (\is_array($value)) { ?>
+        <?php echo \sprintf('%s: %s,', $key, var_export($value, true)); ?>
+    <?php } else { ?>
         <?php echo \sprintf('%s: \'%s\',', $key, $value); ?>
-    <?php endif; ?>
-<?php endforeach; ?>
+    <?php } ?>
+<?php } ?>
 );
 }
 
